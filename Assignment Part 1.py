e67830800559934801efa3dd0516b95486ba5360
Meijden <m.h.m.v.d.meijden@student.tue.nl>
2021-02-15 13:23:34 +0100

    Changed the find_majority function that now selects the smallest difference instead of the smallest label. This lead to also changing a line in the function predict (line 63). Comment; it takes forever to iterate over one k

--- Assignment Part 1.py
+++ Assignment Part 1.py
@@ -5,7 +5,9 @@ Created on Thu Feb  4 13:34:03 2021
 @author: 20172458
 """
 
-import numpy as np    
+import numpy as np
+import pandas as pd
+from tqdm import tqdm #Loadingbar
 
 def euclidean_distance(img_1, img_2):
     '''
@@ -32,16 +34,19 @@ def find_majority_class(labels):
     count_dict = {}
     # Initiate dictionary
     for i in range(10):
-        count_dict[i] = 0
+        count_dict[i] = [0,0]
     
-    for label in labels:
-        count_dict[label] += 1
+    for diff, label in labels:
+        count_dict[label][0] += 1 # Frequency
+        count_dict[label][1] += diff # Total difference
     
-    max_value = max(count_dict.values())  # maximum value
-    max_keys = [k for k, v in count_dict.items() if v == max_value]
+    max_freq = max(count_dict.values(), key=lambda x : x[0])[0] # maximum frequency
+    max_keys = [(label, score) for label, score in count_dict.items() if score[0] == max_freq]
     
-    return sorted(max_keys)[0] # Return the class with lowest label number or we
-                               # Or we can think of a rule that takes into account similar digits
+    if len(max_keys) == 1:
+        return(max_keys[0][0])
+    else:
+        return(min(max_keys, key=lambda x: x[1])[0]) # Return the label with smallest difference
                                
 
 def predict_digits(k, train_images, train_labels, test_image):
@@ -55,7 +60,7 @@ def predict_digits(k, train_images, train_labels, test_image):
     sorted_distances = sorted(distances, key=lambda x: x[0])
     
     # Extract k labels
-    k_labels = [x[1] for x in sorted_distances[:k]]
+    k_labels = sorted_distances[:k]
     
     return find_majority_class(k_labels)
 
@@ -64,19 +69,33 @@ test_data = np.genfromtxt("MNIST_test_small.csv", delimiter=',')
 train_labels, train_images = train_data[:,0], train_data[:, 1:] # Splitting training data
 test_labels, test_images = test_data[:,0], test_data[:, 1:] # Splitting test data
 
-for k in range(1,21):
+empirical_test_loss = []; empirical_train_loss = []
+for k in range(1,2):
     
-    i = 0; loss = 0
-    for test_image in test_images:
+    i_test = 0; loss_test = 0
+    i_train = 0; loss_train = 0
+    for test_image in tqdm(test_images):
+        prediction_test = predict_digits(k, train_images, train_labels, test_image)
         
-        prediction = predict_digits(k, train_images, train_labels, test_image)
-        if prediction != test_labels[i]:
-            loss += 1
-        
-        i += 1
+        if prediction_test != test_labels[i_test]:
+            loss_test += 1
     
-    empirical_loss = loss / len(test_images)
-    print("k: {} - {}".format(k, empirical_loss))
+        i_test += 1
+    for train_image in train_images:
+        prediction_train = predict_digits(k, train_images, train_labels, train_image)
+        
+        if prediction_train != train_labels[i_train]:
+            loss_train += 1
+        
+        i_train += 1
+    
+    empirical_test_loss.append(loss_test / len(test_images))
+    empirical_train_loss.append(loss_train / len(train_images))
+    
+
+df_results_qa = pd.DataFrame(data = {'k': list(range(1,21)), 'Empirical Training Loss': empirical_train_loss,
+                              'Empirical Test Loss': empirical_test_loss})
+df_results_qa.to_csv("results_q1a", index = False)
 
         
         
